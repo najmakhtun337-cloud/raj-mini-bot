f3f8c5f0ecbc8d0b6b70f29b83de281a5806fc1e
DARK-ERRO <najmakhtun337@gmail.com>
2025-12-25 16:27:21 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  botName: "Raj",                     // Bot name
+  ownerNumber: "+9190465579718",       // তোমার WhatsApp number
+  groupLink: "https://chat.whatsapp.com/Hw0JIQgGHco8BL6699",
+  channelLink: "https://whatsapp.com/channel/0029VbBQQ6v4Y9lenR8ROD3O",
+  profileImage: "https://files.catbox.moe/j3ih0d.jpg"
+};
